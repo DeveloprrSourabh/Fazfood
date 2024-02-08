4af2f314480138e14345c58615f656be673d7e06
Sourabh Sain <114208070+DeveloprrSourabh@users.noreply.github.com>
2024-02-08 18:35:55 +0530

    Product and Category update delete functionality done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -47,17 +47,62 @@ exports.updateCategoryController = async (req, res) => {
   try {
     const { name } = req.fields;
     const { photo } = req.files;
+    const { id } = req.params;
     if (!name) {
       return res.status(200).send({
         success: false,
         message: "Name is required",
       });
     }
+    const existName = await Category.findOne({ name });
+    if (existName) {
+      return res.status(400).send({ message: "Category Already Exist" });
+    } else {
+      let cat = await Category.findByIdAndUpdate(
+        id,
+        {
+          name: name,
+          slug: slugify(name),
+        },
+        { new: true }
+      );
+    }
+    return res.status(200).send({
+      success: true,
+      message: "Category Updated Successfully",
+      cat,
+    });
   } catch (error) {
     console.log(error);
     return res.status(200).send({
       success: false,
       message: "Error While Update Category",
     });
+  }
+};
+
+// Delete Category Controller
+
+exports.deleteCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const cat = await Category.findByIdAndDelete(id);
+    return res
+      .status(200)
+      .send({ success: true, message: "Category Deleted Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+      message: "Error While Deleting Category",
+    });
+  }
+};
+
+// Getting All Products
+exports.getAllProducts = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
   }
 };
--- controllers/productController.js
+++ controllers/productController.js
@@ -32,12 +32,6 @@ exports.addProductController = async (req, res) => {
         message: "Price Is required",
       });
     }
-    if (!quantity) {
-      return res.status(200).send({
-        succes: false,
-        message: "Quantity Is required",
-      });
-    }
     if (!offer) {
       return res.status(200).send({
         succes: false,
@@ -74,6 +68,100 @@ exports.addProductController = async (req, res) => {
     return res.status(400).send({
       success: false,
       message: "Error While Adding Product",
+    });
+  }
+};
+
+// Update Product Controller
+exports.updateProductController = async (req, res) => {
+  try {
+    const { name, description, price, quantity, category, offer, slug } =
+      req.fields;
+    const { id } = req.params;
+    const { photo } = req.files;
+    if (!name) {
+      return res.status(200).send({
+        succes: false,
+        message: "Name Is required",
+      });
+    }
+    if (!description) {
+      return res.status(200).send({
+        succes: false,
+        message: "Description Is required",
+      });
+    }
+    if (!price) {
+      return res.status(200).send({
+        succes: false,
+        message: "Price Is required",
+      });
+    }
+    if (!offer) {
+      return res.status(200).send({
+        succes: false,
+        message: "Offer Is required",
+      });
+    }
+    if (!quantity) {
+      return res.status(200).send({
+        succes: false,
+        message: "Quantity Is required",
+      });
+    }
+    if (!photo) {
+      return res.status(200).send({
+        succes: false,
+        message: "Photo Is required",
+      });
+    }
+    const existsProduct = await Product.findById(id);
+    if (existsProduct.slug == slug) {
+      return res.status(400).send({
+        success: false,
+        message: "Choose Different Product Name",
+      });
+    }
+    let product = await Product.findByIdAndUpdate(
+      id,
+      {
+        ...req.fields,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
+    }
+    await product.save();
+    return res.status(200).send({
+      success: true,
+      message: "Product Updated Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+      message: "Error While Updating Product",
+    });
+  }
+};
+
+// Delete Product Controller
+exports.deleteProductController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    return res.status(200).send({
+      success: true,
+      message: "Product Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+      message: "Error While Deleting Proudct",
     });
   }
 };
